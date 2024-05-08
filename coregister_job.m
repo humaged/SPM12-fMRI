587d24c844fa0c1ccf5dbea159dde7243f8f5d58
humaged <154232340+humaged@users.noreply.github.com>
2024-05-08 13:26:19 +0200

    Add files via upload

---
+++ coregister_job.m
@@ -0,0 +1,12 @@
+%-----------------------------------------------------------------------
+% Job saved on 08-May-2024 12:14:43 by cfg_util (rev $Rev: 7345 $)
+% spm SPM - SPM12 (7771)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.spatial.coreg.estimate.ref = {'/Users/humagedemenli/Documents/data/MoAEpilot/fM00223/meanfM00223_016.img,1'};
+matlabbatch{1}.spm.spatial.coreg.estimate.source = {'/Users/humagedemenli/Documents/data/MoAEpilot/sM00223/sM00223_002.img,1'};
+matlabbatch{1}.spm.spatial.coreg.estimate.other = {''};
+matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.cost_fun = 'nmi';
+matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.sep = [4 2];
+matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.tol = [0.02 0.02 0.02 0.001 0.001 0.001 0.01 0.01 0.01 0.001 0.001 0.001];
+matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.fwhm = [7 7];
